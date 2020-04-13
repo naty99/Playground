0b5986e27a7b77194747a20d61b30321fc40ac13
Thijs Nulle <thijsnulle@Thijs-MacBook-Pro.local>
2020-04-13 15:44:54 +0200

    better than everything

--- Pong/Pong/Planet.swift
+++ Pong/Pong/Planet.swift
@@ -14,15 +14,16 @@ class Planet: SKNode {
     override init() {
         super.init()
         
-        let radius: CGFloat = CGFloat.random(in: 20...30)
+        let radius: CGFloat = CGFloat.random(in: 15...25)
         let shape = SKShapeNode(circleOfRadius: radius)
         shape.fillColor = UIColor.white
         
         self.physicsBody = SKPhysicsBody.init(circleOfRadius: radius)
         self.physicsBody?.affectedByGravity = false
         
-        for i in 1...5 {
-            let circle = SKShapeNode(circleOfRadius: radius * CGFloat(i))
+        for i in 0...10 {
+            let circle = SKShapeNode(circleOfRadius: radius + CGFloat(i + 1) * radius / 2)
+            circle.strokeColor = UIColor(hue: 0, saturation: 0, brightness: 1, alpha: 1 - 0.1 * CGFloat(i))
             self.addChild(circle)
         }
         
